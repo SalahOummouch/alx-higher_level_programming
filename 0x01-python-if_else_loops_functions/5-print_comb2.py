8d4fcbbef6c426534ce5167315782f81cdccac31
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-03 12:53:02 -0700

    if else

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number < 10:
+        print("0", number, ",", end="")
+    elif:
+        print("99")
+    else:
+        print(number, ",", end="")
