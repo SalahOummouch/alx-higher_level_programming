df4ef7d89f47a60151ce5a5d0a4b0a8cb452a2b2
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-03 08:29:45 -0700

    if else

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char_ascii in range(ord('c'), ord('z') + 1):
-    print("{}".format(chr(char_ascii)), end = '')
+for char_ascii in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(char_ascii)), end='')
