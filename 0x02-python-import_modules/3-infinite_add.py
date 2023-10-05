4d7b247ff3796302496356743ecb0ee3c57f4c88
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-05 08:25:49 -0700

    import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    number=len(sys.argv) - 1
+    number = len(sys.argv) - 1
     sum = 0
     for index in range(number):
-        sum+=int(sys.argv[index + 1])
+        sum += int(sys.argv[index + 1])
     print(sum)
