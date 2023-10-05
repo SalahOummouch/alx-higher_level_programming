73d1824e6e15da4d1c97c648cdc0903bddecba29
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-05 07:48:04 -0700

    import module

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(sys.argv) - 1 == 1:
+        print("1 argumrnt:")
+    else:
+        print("{} arguments:".format(count))
+    for index in range(len(sys.argv)):
+        print("{}: {}".format(index + 1, sys.argv[index + 1]))
