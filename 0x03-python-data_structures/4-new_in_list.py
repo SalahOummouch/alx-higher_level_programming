55912b026c935a911ee838ea7ab179c07d4ac909
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-08 12:56:00 -0700

    data structure

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        new_list = []
+        for i in range(len(my_list)):
+            new_list.append(my_list[i])
+            if i == idx:
+                new_list[i] = element
+    return new_list
