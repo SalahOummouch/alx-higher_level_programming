c1eb46427ef89c414d4041522fe71ed03cca6815
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-09 13:58:42 -0700

    data structure

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    for i in range(len(my_string):
+        if my_string[i] == "c" or my_string[i] == "C":
+            my_string[i] = " "
+        return my_string
+
+            
