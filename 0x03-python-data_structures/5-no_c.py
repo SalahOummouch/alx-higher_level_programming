897249cfee4f11464bd79dd5af9023ae012b76bc
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-09 14:09:44 -0700

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,9 @@
 def no_c(my_string):
     new_string = ""
     j = 0
-    for i in range(len(my_string):
-        if my_string[i] != "c" or my_string[i] != "C":
-            new_string[j] = my_string[i]
-            j++
+    for i in range(len(my_string)):
+        if my_string[i] != "c" and my_string[i] != "C":
+            new_string += my_string[i]
+            j += 1
     return new_string
-            
+
