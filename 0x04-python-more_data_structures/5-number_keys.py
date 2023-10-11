84c879687bf3f7177b796cdcfd87bad78e4f9f89
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-11 07:42:09 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 # 5. Number of keys
 def number_keys(a_dictionary):
-    '''function that returns the number of keys in a dictionary.'''
     return len(a_dictionary)
--- 6-print_sorted_dictionary.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# 6. Print sorted dictionary
-def print_sorted_dictionary(a_dictionary):
-    '''function that prints a dictionary by ordered keys.'''
-
