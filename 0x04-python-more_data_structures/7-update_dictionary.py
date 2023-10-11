ee4469ba58c228f1dfdfdc13da4e9d8667554c13
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-11 13:35:35 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,4 +4,3 @@ def update_dictionary(a_dictionary, key, value):
     '''Write a function that replaces or adds key/value in a dictionary.'''
     a_dictionary[key] = value
     return (a_dictionary)
-
