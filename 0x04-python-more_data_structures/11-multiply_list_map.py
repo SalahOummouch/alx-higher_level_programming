cfb60cd64546deb35134dda37237567820ff9062
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-11 13:49:40 -0700

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 11-mutiply_list_map.py
+def multiply_list_map(my_list=[], number=0):
+    '''function that returns a list with all 
+    values multiplied by a number without using any loops.'''
+    return (list(map((lambda i: i * number), my_list)))
