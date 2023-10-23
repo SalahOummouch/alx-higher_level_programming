b2c0d72c74a015e675d8d6a42e5763498df48e54
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-23 09:20:52 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -9,3 +9,18 @@ nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
+import pycodestyle
+
+def check_pep8_compliance(filename):
+    style_guide = pycodestyle.StyleGuide()
+    result = style_guide.check_files([filename])
+    return result.total_errors
+
+filename = "0-safe_print_list.py"  # Remplacez par le nom de votre fichier
+num_errors = check_pep8_compliance(filename)
+
+if num_errors == 0:
+    print(f"The code in {filename} is PEP 8 compliant.")
+else:
+    print(f"{num_errors} PEP 8 violations found in {filename}.")
+
--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if elements_printed < x:
-                print(i, end = "")
+                print(i, end="")
                 elements_printed += 1
         print("")
-    except:
+    except Exception as e:
         pass
     return elements_printed
