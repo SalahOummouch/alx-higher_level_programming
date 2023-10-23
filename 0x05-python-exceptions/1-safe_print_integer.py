68086dcda8d3ce7ee9bdeb6a8a86de7cbe0ff156
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-23 09:41:17 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         new_value = int(value)
-        print(new_value)
+        print("{}".format(new_value))
         return True
     except Exception as e:
         return False
