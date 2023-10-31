2a3e3e36d259ec493d72f25bea9a5f85792a38b2
SalahOummouch <127281160+SalahOummouch@users.noreply.github.com>
2023-10-31 15:42:55 +0100

    Add files via upload

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""Module for add_integer method."""
+
 def add_integer(a, b=98):
-    '''function that adds 2 integers.
+    """Adds two integers.
 
     Args:
-        a: first integer
-        b: seconde integer ( by default = 98)
+        a: the first integer.
+        b: the second integer, default 98.
 
     Raises:
         TypeError: if a, b are not int, float.
 
     Returns:
         The sum of the two integers.
-    '''
+    """
+
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
     if type(b) not in (int, float):
