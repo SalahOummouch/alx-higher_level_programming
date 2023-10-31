d0a055f769a3808ab45650d1e0aadd97850a24da
SalahOummouch <s.oummouch@enim.ac.ma>
2023-10-31 07:35:27 -0700

    alx-higher_level_programming

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    '''function that adds 2 integers.
+
+    Args:
+        a: first integer
+        b: seconde integer ( by default = 98)
+
+    Raises:
+        TypeError: if a, b are not int, float.
+
+    Returns:
+        The sum of the two integers.
+    '''
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
