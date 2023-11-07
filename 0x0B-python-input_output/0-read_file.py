9afec6f0f6b6f9b27aa16ed58253b0f9e0a8dc13
SalahOummouch <s.oummouch@enim.ac.ma>
2023-11-07 15:36:38 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,4 +4,5 @@ def read_file(filename=""):
     '''function that reads a text file (UTF8)
     and prints it to stdout'''
     with open('filename', encoding="utf-8") as f:
-        read_data = f.read()
+        for line in f:
+            print(line, end="")
